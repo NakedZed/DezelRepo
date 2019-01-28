b591b8e3ce47a828e8bebf8c76a630ae56328771
zed <manoa15@yahoo.com>
2019-01-28 17:24:23 +0200

    Add GET /Users route

--- Server/server.js
+++ Server/server.js
@@ -7,23 +7,33 @@ var {User} = require('./models/Users')
 var app = express();
 app.use(bodyParser.json());            //configuring bodyparser middleware to get the body of the request.
 
-
-
-app.post('/Users',(req,res)=>{    //Handling post Route for adding a todo
+//Handling post Route for adding a todo
+///////////////////////////////////////
+app.post('/Users',(req,res)=>{
   var User1 = new User({
     name:req.body.name,
     email:req.body.email
   })
 
-  User1.save().then((doc)=>{
+  User1.save().then((user)=>{   //Saving the user to the dezelDB
 
-res.send(doc)
+res.send(user)
 
 },(err)=>{
   console.log('something went wrong')
 });
 })
+app.get('/Users',(req,res)=>{
+  User.find().then((user)=>{
+    res.send(user);
+  })
+},(err)=>{
+     res.status(400).send(e)
+})
 
+
+
+/////////////////////////////////////////////////////
 app.listen(3000,()=>{                  //listening to the port
   console.log('Started on port 3000')
 });
